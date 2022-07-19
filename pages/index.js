de525540052b14b68477737b30ea88a9c662993e
Higor Pires <higorlpires@gmail.com>
2022-07-19 18:21:45 -0300

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import {HeroBanner,FooterBanner} from '../components/index'
 
 
 
 const Home = () => {
   return (
     <div>
-      
+      <HeroBanner/>
     </div>
   )
 }
