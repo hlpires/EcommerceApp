8abb806ee7b2576954c8ca26ceb330373e7b6055
Higor Pires <higorlpires@gmail.com>
2022-07-19 17:37:44 -0300

    commit

--- components/index.js
+++ components/index.js
@@ -1,1 +1,7 @@
-export {default as Header} from './Header'
+export {default as Cart} from './Cart'
+export {default as Footer} from './Footer'
+export {default as FooterBanner} from './Cart'
+export {default as HeroBanner} from './Cart'
+export {default as Layout} from './Cart'
+export {default as Navbar} from './Cart'
+export {default as Product} from './Cart'
