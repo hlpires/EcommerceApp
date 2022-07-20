d0709d52d6c810dd9f22c54534f43a83b062ea04
Higor Pires <higorlpires@gmail.com>
2022-07-20 19:37:21 -0300

    commit

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
-
+import semjuros from '../img/semjuros.png'
 const Banner = () => {
   return (
     <div className = 'banner'>     
         <div className = 'position'>
             <div className = 'bannerBox'>
+                <div className = 'bannerImg'></div>
+                <div className = 'bannerItens'>
+                    <div className ='bannerItem' id ='bannerItem1'> </div>
+                    <div className ='bannerItem' id ='bannerItem2'> </div>
+                    <div className ='bannerItem' id ='bannerItem3'> </div>
+                    <div className ='bannerItem' id ='bannerItem4'> </div>
+                </div>
 
             </div>
         </div>
