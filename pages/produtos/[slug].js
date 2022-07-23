086ec0f3af8336ca556384ebf0be41203b7c67d1
Higor Pires <higorlpires@gmail.com>
2022-07-23 20:27:01 -0300

    commit

--- pages/produtos/[slug].js
+++ pages/produtos/[slug].js
@@ -2,13 +2,17 @@ import React from 'react'
 import {Header} from '../../components/index'
 const detalhesProdutos = () => {
   return (
+      
     <div>
         <Header/>
-        <div className = 'detlahesBox'>
-            <div className = 'datelhesImg'></div>
+      <div className = 'detalhes'>
+      <div className = 'positionDetalhes'>
+        <div className = 'detalhesBox'>
+            <div className = 'datalhesImg'></div>
             <div className= 'detalhesText'></div>
-
         </div>
+      </div>
+      </div>
     </div>
   )
 }
