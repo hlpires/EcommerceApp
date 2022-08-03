60fab0ed466a83f2756c0264177863c6b1d74619
Higor Pires <higorlpires@gmail.com>
2022-08-03 07:09:21 -0300

    commit

--- pages/produtos/[slug].js
+++ pages/produtos/[slug].js
@@ -213,14 +213,14 @@ const handleCheckout = async () => {
       <div className ='produtosdois'>
         
             <div className ='produtosdoisbox1'>
-                <div className = 'produtosdoisItem' > <a href = '/'><img className='produtosDoisImg' src={urlFor(produtos.produtos[2].imagem?.[0])} alt="" /></a></div>
-                <div className = 'produtosdoisItem' ><a href = '/'><img className='produtosDoisImg' src={urlFor(produtos.produtos[0].imagem?.[0])} alt="" /></a></div>
-                <div className = 'produtosdoisItem' ><a href = '/'><img className='produtosDoisImg' src={urlFor(produtos.produtos[3].imagem?.[0])} alt="" /></a></div>
-                <div className = 'produtosdoisItem' ><a href = '/'><img className='produtosDoisImg' src={urlFor(produtos.produtos[4].imagem?.[0])} alt="" /></a></div>
-                <div className = 'produtosdoisItem' ><a href = '/'><img className='produtosDoisImg' src={urlFor(produtos.produtos[5].imagem?.[0])} alt="" /></a></div>
-                <div className = 'produtosdoisItem' ><a href = '/'><img className='produtosDoisImg' src={urlFor(produtos.produtos[6].imagem?.[0])} alt="" /></a></div>
-                <div className = 'produtosdoisItem' ><a href = '/'><img className='produtosDoisImg' src={urlFor(produtos.produtos[8].imagem?.[0])} alt="" /></a></div>
-                <div className = 'produtosdoisItem'><a href = '/'><img className='produtosDoisImg' src={urlFor(produtos.produtos[9].imagem?.[0])} alt="" /></a></div>
+                <div className = 'produtosdoisItem' > <img className='produtosDoisImg' src={urlFor(produtos.produtos[2].imagem?.[0])} alt="" /></div>
+                <div className = 'produtosdoisItem' ><img className='produtosDoisImg' src={urlFor(produtos.produtos[0].imagem?.[0])} alt="" /></div>
+                <div className = 'produtosdoisItem' ><img className='produtosDoisImg' src={urlFor(produtos.produtos[3].imagem?.[0])} alt="" /></div>
+                <div className = 'produtosdoisItem' ><img className='produtosDoisImg' src={urlFor(produtos.produtos[4].imagem?.[0])} alt="" /></div>
+                <div className = 'produtosdoisItem' ><img className='produtosDoisImg' src={urlFor(produtos.produtos[5].imagem?.[0])} alt="" /></div>
+                <div className = 'produtosdoisItem' ><img className='produtosDoisImg' src={urlFor(produtos.produtos[6].imagem?.[0])} alt="" /></div>
+                <div className = 'produtosdoisItem' ><img className='produtosDoisImg' src={urlFor(produtos.produtos[8].imagem?.[0])} alt="" /></div>
+                <div className = 'produtosdoisItem'><img className='produtosDoisImg' src={urlFor(produtos.produtos[9].imagem?.[0])} alt="" /></div>
             </div>
         
     </div>
